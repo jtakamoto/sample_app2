67520db4fb591d8088fedd140f5749633f1c39ed
Joji Takamoto <jojitakamoto@gmail.com>
2016-10-12 13:28:11 -0400

    1st save

---
+++ README.md
@@ -0,0 +1,5 @@
+# Intro to Programming with Python
+
+This is a sample application
+
+by [Mattan Griffel](http://mattangriffel.com)
---
+++ giphy.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+import giphypop
+app = Flask(__name__)
+
+def get_stock_price(ticker):
+    quotes = getQuotes(ticker)
+    price = quotes[0]['LastTradePrice']
+    return "The price of {} is {}".format(ticker, price)
+
+@app.route('/')
+def index():
+    name = request.values.get('name', 'Nobody')
+    greeting = "Hello {}".format(name)
+    return render_template('index.html', greeting=greeting)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/results')
+def results():
+    stock = request.values.get('stock')
+    g = giphypop.Giphy()
+    results = g.search(stock) 
+    return render_template('results.html', results=results)
+
+app.run(debug=True)
+
+
+
+
+
+
+# returns a list of objects
+#results = g.search('cats') 
+
+
+    #print(result.media_url)
+    #print(result.url)
+
